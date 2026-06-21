e38d44c07fea0513c7a9e64f72fd44c0273b323e
Peter J <push.peji@gmail.com>
2026-06-20 22:57:27 -0400

    Add PWA support, offline caching, and cute n-1 mascot icon

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'n1-word-craft-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './words_data.js',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request);
+    })
+  );
+});
